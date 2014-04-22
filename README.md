ed3ef360238b54ed28e900bfe07b91d709d5d93c
C J Silverio <ceej@npmjs.com>
2014-04-22 12:35:13 -0700

    Implemented client login and some tests for it. One step forward!

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ dynector
 [API docs](https://help.dynect.net/dns-api-knowledge-base/)
 
 
+
+## Unit tests
+
+To run the tests you must set the following environment variables to your Dynect account credentials:
+
+
+```
+DYN_CUSTOMER
+DYN_USER
+DYN_PASSWORD
+```
+
+I'll mock the API at some point for the bulk of the tests, but the integration tests will probably always need credentials of some kind.
+
 ## License
 
 [ISC](http://opensource.org/licenses/ISC)
--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-// TODO insert code here
+var
+    _       = require('lodash'),
+    assert  = require('assert'),
+    events  = require('events'),
+    P       = require('bluebird'),
+    Request = require('request'),
+    util    = require('util')
+    ;
+
+
+var BASE = 'https://api.dynect.net/REST/';
+
+var Dynector = module.exports = function Dynector(opts)
+{
+    events.EventEmitter.call(this);
+
+};
+util.inherits(Dynector, events.EventEmitter);
+
+Dynector.prototype.token = null;
+
+
+// public API: all methods can either take a callback or return a promise
+
+Dynector.prototype.login = function(credentials, callback)
+{
+    return this._login(credentials).nodeify(callback);
+};
+
+
+// promises implementations
+
+Dynector.prototype._login = function _login(credentials)
+{
+    assert(credentials && _.isObject(credentials), 'you must pass a credentials object to login()');
+    assert(credentials.customer_name && _.isString(credentials.customer_name), 'customer_name is required in the login credentials');
+    assert(credentials.user_name && _.isString(credentials.user_name), 'user_name is required in the login credentials');
+    assert(credentials.password && _.isString(credentials.password), 'password is required in the login credentials');
+
+    var self = this;
+
+    var opts =
+    {
+        uri    : BASE + 'Session/',
+        method : 'POST',
+        json   : credentials,
+    };
+
+    return self.execute(opts)
+    .then(function(reply)
+    {
+        if (reply.status === 'success')
+        {
+            self.token = reply.data.token;
+            self.emit('log', 'logged in');
+            return true;
+        }
+
+        self.emit('log', 'login failure: ' + reply);
+        return false;
+    });
+};
+
+
+// promises wrapper for request
+Dynector.prototype.execute = function execute(opts)
+{
+    var self = this,
+        deferred = P.defer();
+
+    Request(opts, function(err, response, body)
+    {
+        if (err) return deferred.reject(err);
+        deferred.resolve(body);
+    });
+
+    return deferred.promise;
+}
